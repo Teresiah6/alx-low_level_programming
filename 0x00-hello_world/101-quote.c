6f6de8ee8525da315b82c5153cddaab89024eed4
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 15:34:20 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main block-int return type
+ * return 1 as opposed to zero
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}

ee67ac9475c8843ac33a085968e190d7d58d35a3
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 16:12:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 /**
- * main block- Entry point
+ * Function main- main block- Entry point
  *
  *Description: Putting "Programming is like building a multilingual puzzle"
  * 
- * returns zero -success
+ * return value of main- zero -success
  */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
 

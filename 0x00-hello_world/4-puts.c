2c4fb08dd4c880759e87877c1e69088d99732ded
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 16:04:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,15 @@
 #include<stdio.h>
 /**
- * main -main block of type int
- * use void as no arguments arguments
+ * main block- Entry point
+ *
+ *Description: Putting "Programming is like building a multilingual puzzle"
  * 
- * main: return type of int returns 0
+ * returns zero -success
  */
 int main(void)
 {
-	/**
-	 * Puts -command used 
-	 *
-	 */
 
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 * returns zero -exit point
-	 *
-	 */
 	return (0);
 
 }

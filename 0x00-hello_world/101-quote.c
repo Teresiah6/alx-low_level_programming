9dec2af725c4cb2bf58ffbe2ca8eaf491f5bd280
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 16:28:27 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
 #include<unistd.h>
 /**
- * main block-int return type
+ * main function - prints a sentecefollowed by a new line to standard error
  *
- * description: write a sentence
- *
- * return 1 as opposed to zero
+ * return alway return 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

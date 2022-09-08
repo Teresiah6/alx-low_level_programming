137d6e740071deaa8d8a43197bb411acdf1b5984
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 14:59:31 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 /**
  * Main return type int and no arguments therefore we use void
- * Main block
- * main return type void
- * return 0
+ * * return 0
  * use \n for new line
  * main returns int
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/**
-	 * Return type is zero, nothing is returned
-	 * 
-	 */
 	return (0);
 }

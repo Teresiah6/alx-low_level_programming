9fb19d4b8c6d98cbbb97c0a5ba7fbd87b16812c0
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-08 16:23:11 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,13 @@
 #include<unistd.h>
 /**
  * main block-int return type
+ *
+ * description: write a sentence
+ *
  * return 1 as opposed to zero
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

6197d4cbacf0bf4eb6380c9678c6f799c4ce85e1
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-09 12:45:10 -0700

    alphabet except q & e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	char x;
 
-	for (x ='a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		if (x == 'q')
 			continue;
-		else if(x == 'e')
+		else if (x == 'e')
 			continue;
 	putchar(x);
 	}
-	
+
 	putchar('\n');
 
 	return (0);

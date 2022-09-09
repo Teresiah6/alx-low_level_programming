08d7ef1369ba9e4254a6829c3a27d554d6105518
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-09 13:41:10 -0700

    print number using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,15 +6,9 @@
  *
  * Return: always o
  */
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(void)
 {
 	int i;
-
 	
 	for (i = 48 ; i <= 57; i++)
 	{

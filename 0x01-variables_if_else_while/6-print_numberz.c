366f1da1f863433ecf1a390e23118a6f9953b9ff
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-09 13:32:34 -0700

    print number using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main -program that prints all single
+ *
+ * digit number of base 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 57; i++)
+	{
+		putchar (i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}

f2d0e82f69ea0584c3ae7f072050a41479093c64
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-09 14:29:14 -0700

    8&9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,12 @@ int main(void)
 {
 	char i;
 	
-	for (i = 48; i <= 57; i++) 
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (i =97; i <102; i++)
 	{
 		putchar(i);
 	}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - program that prints all
+ *
+ * possible combinations of single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <=57; i++)
+	{
+		putchar(i);
+		if(i == 57)
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	 
+
+	return (0);
+}

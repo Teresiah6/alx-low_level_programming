73dd33f0ee53d600c621329996bc1c0ce430856d
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-09 12:28:37 -0700

    print alphabets: q

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - program that prints the
+ *
+ * alphabet in lowercase then upper
+ *
+ * Return : always 0
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+		
+
+		for (y = 'A'; y <= 'Z'; y++)
+		{
+			putchar(y);
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}

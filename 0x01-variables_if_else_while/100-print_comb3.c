d165e8fd4b296c514a948fae8f84cef1cd16b638
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-11 14:35:02 -0700

    2 digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - program that prints all 
+ * main - program that prints all
  *
  * possible different combinations of 2 digits
  *
@@ -13,12 +13,18 @@ int main(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <10; num2++)
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if(num1 == 9 && num2 == 9)
+			if(num2 == 0)
+			{
+				break;
+			}
+
+
+			if (num1 == 8 && num2 == 9)
 				continue;
 			putchar(',');
 		}

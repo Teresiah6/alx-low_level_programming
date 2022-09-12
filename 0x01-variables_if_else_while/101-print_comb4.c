8199314697b9ad802694a5fa11a3610dbf8bb774
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-12 09:30:59 -0700

    trippledigits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - program that prints all possible
+ *
+ * different combinations of 3 digits
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	int i;
+	int j;
+	int k;
+
+	for (i ='0'; i<'8'; i++)
+	{
+		for (j = i+1; j < '9'; j++)
+		{
+			for (k = j+1; k <= '9'; k++)
+			{
+				if ((i !=j) !=k)
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+				if (i == '7' && j == '8' && k == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

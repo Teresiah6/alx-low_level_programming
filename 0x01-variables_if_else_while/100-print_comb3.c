c6125665810e8c7849958d3e6e2d284283590641
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-12 09:11:47 -0700

    double digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 	int num1;
 	int num2;
 
-	for (num1 = '0'; num1 < 9; num1++)
+	for (num1 = '0'; num1 < '9'; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= '9'; num2++)
 		{
@@ -20,7 +20,7 @@ int main(void)
 				putchar(num1);
 				putchar(num2);
 
-				if (num1 == '8' && num2 =='9')
+				if (num1 == '8' && num2 == '9')
 					continue;
 
 				putchar(',');

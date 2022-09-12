27a8b860819e4f68a77fb6a199a0890662b6745b
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-12 09:40:23 -0700

    trippledigits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,26 @@
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	int i;
 	int j;
 	int k;
 
-	for (i ='0'; i<'8'; i++)
+	for (i = '0'; i <'8'; i++)
 	{
-		for (j = i+1; j < '9'; j++)
+		for (j = i + 1; j < '9'; j++)
 		{
-			for (k = j+1; k <= '9'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
-				if ((i !=j) !=k)
+				if ((i != j) != k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
 
 					if (i == '7' && j == '8' && k == '9')
-						continue;
-					
+						continue;					
 					putchar(',');
 					putchar(' ');
 				}

3fe2de4b27178d04f23d8e7eb85a273f386aa0f8
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 00:34:48 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,8 +8,8 @@
  */
 int _isalpha(int c)
 {
-	if((c > 64 && c <= 90) || (c > 96 && c <= 122))
-		return(1);
+	if ((c > 64 && c <= 90) || (c > 96 && c <= 122))
+		return (1);
 	else
-		return(0);
+		return (0);
 }

e37036fbc62a9568849cf744cca1ddc60088f647
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 07:41:07 -0700

    longest day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
  * jack_bauer - entry point
  *
- * Description: prints every minute of the day 
+ * Description: prints every minute of the day
  *
  * Return: Always 0
  */
@@ -11,12 +11,12 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0; h < 24; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; m <60; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar ((h / 10) + '0');
-			_putchar ((h / 10) + '0');
+			_putchar ((h % 10) + '0');
 			_putchar (':');
 			_putchar ((m / 10) + '0');
 			_putchar ((m % 10) + '0');

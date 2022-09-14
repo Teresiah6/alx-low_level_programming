87d2f7c678c4a330452d7eeae7d3905c461a409f
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 01:22:03 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -12,11 +12,13 @@ int _abs(int r)
 	if (r < 0)
 	{
 		r = r * -1;
-		_putchar ('r');
+		_putchar (r);
+		_putchar ('\n');
 	}
 	else
 	{
-		_putchar ('r');
+		_putchar (r);
+		_putchar ('\n');
 	}
 
 	return (0);

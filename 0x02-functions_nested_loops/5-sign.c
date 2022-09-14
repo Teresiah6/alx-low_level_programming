c7016960e28154814db8d95f351af444e428c6f9
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 06:41:27 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar ('+');
+		return (1);
 	}
-	else if (n == 0)
+	else if (n == -1)
 	{
-		return (0);
 		_putchar ('0');
+		return (1);
 	}
 	else
 	{
-		return (-1);
 		_putchar ('-');
+		return (-1);
 	}
 
 }

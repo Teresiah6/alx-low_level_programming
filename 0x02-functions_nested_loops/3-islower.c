373c6082a0710e4f877c705e09f21594d9437346
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 00:21:45 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,7 @@
 /**
  * _islower - entry point, int input type and int arg
  *
- * c - is int that determines if letter is upper or lowercase
- *
- * Description: checks for lowercase character and returns diff int
+ * Description: int c checks for lowercase character and returns diff int
  *
  * Return: either 1 or 0
  */

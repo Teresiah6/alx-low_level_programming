7deb3be977d7fbfa2e6aa23a0a7c61b0197870a3
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 01:15:44 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs - entry point, computes absolute value of r
+ *
+ * @r: int to absolute number
+ *
+ * Return: always 0;
+ */
+int _abs(int)
+{
+	int r;
+
+	if (r < 0)
+	{
+		r = r * -1;
+		_putchar ('r');
+	}
+	else
+	{
+		_putchar ('r');
+	}
+
+	return (0);
+}

c247bbfcf339483b35d2165f6d59dcad724fe586
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-13 23:33:31 -0700

    alphabetX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 : entry point
+ *
+ * Description: prints alphabet ten times
+ *
+ * return: always none
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+	while (i < 10)
+	{
+		for ( c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+			i++;
+	}
+
+	_putchar ('\n')'
+}

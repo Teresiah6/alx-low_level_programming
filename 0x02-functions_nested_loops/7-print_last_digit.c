168a500392b9892ce8a3bc89460c8f82554b85ca
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-14 01:32:21 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * @r: last digit of no. to be printed
+ *
+ * Return: always 0
+ */
+int print_last_digit(int r)
+{
+	r = r % 10;
+
+	_putchar (r);
+
+	return (0);
+
+}

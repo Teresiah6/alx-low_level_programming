c1d02ce1e1c82c45f47cee1a13b7f6c3a7970539
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-13 22:31:32 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - a function that prints teh aphabet in lowercase
+ *
+ * followed by a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+
+	return(0);
+}

3b7ee6e8bb818f1a3981313fa660084845d5b5cc
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-15 11:43:07 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,11 +10,17 @@ int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 	{
+		_putchar (c);
+		_putchar (':');
 		return (1);
 	}
 	else 
 	{
+		_putchar (c);
+		_putchar (':');
 		return (0);
 	}
+
+	_putchar ('\n');
 
 }

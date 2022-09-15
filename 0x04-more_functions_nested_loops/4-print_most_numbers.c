76622f235c676c541b0c8937841aec03710f6106
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-15 12:47:11 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ *
+ * @i: prints no.s 0 to 9 less 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ( (i != 2) && (i != 4))
+		{
+			continue;
+		}
+
+		else 
+			break;
+
+				
+					
+	}
+
+	_putchar (i);
+	_putchar ('\n');
+}

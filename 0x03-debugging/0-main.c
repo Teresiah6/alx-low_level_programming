17def0d5857ec3c58782148938cbc4105484e8a4
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-15 13:50:51 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * positive or negative - entry point
+ *
+ * @i : checks if + or -
+ *
+ * Return: always 0
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf ("%i is positive\n", i);
+	}
+	else  if (i < 0)
+	{
+		printf ("%i is negative\n", i);
+	}
+	else 
+	{
+		printf("%i is zero\n", i);
+	}
+
+}

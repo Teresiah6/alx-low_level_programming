b36afc9c9a2a1d1dc0a9ec4e2d42d4cbf59705ea
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-16 14:04:06 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,7 +8,7 @@
  */
 int _isdigit(int c)
 {
-	if (( c >= 0) && (c <= 9))
+	if (( c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
@@ -16,4 +16,5 @@ int _isdigit(int c)
 	{
 		return (0);
 	}
+	_putchar ('\n');
 }

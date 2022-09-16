ce07adff5a2984ed0f3e9ef18c54c86af06ba2d9
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-16 14:40:11 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,17 +12,15 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if ( (i != 2) && (i != 4))
+		if ((i != 2) && (i != 4))
 		{
-			_putchar (i);
-
+			continue;
 		}
 
-		else 
-			continue;
+		else
+			break;
 
-				
-					
+		_putchar (i);
+		_putchar ('\n');
 	}
-	_putchar ('\n');
 }

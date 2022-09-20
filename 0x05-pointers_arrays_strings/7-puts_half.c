cf5810124d916491c837e399f359f15b35507f5e
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-20 14:09:18 -0700

    7&8

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _strlen - length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\n')
+	{
+		l++;
+		s++;
+
+	}
+	return(l);
+}
+
+/**
  * puts_half - entry point
  * @str: 2nd half of the string to be printed
  * @n: if no. of char is odd n =(length-1)/2
@@ -7,26 +25,22 @@
  */
 void puts_half(char *str)
 {
-	int i;
-	int length = 0;
-	int n;
+	int it;
+	int l = _strlen(str);
 
-	for (i = 0; str != '\0'; i++)
+
+	if ((l % 2) != 0)
 	{
-		length++;
-
+		it = ((l / 2) + 1);
+	}
+	else
+	{
+		it = (l / 2);
 	}
 
-	n = (length / 2);
-
-	if ((length % 2) == 1)
+	while (it < l)
 	{
-		n = ((length + 1) / 2);
-	}
-
-	for (i = n; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
+		_putchar(*(str+ it));
 	}
 
 	_putchar('\n');
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 	{
 		if (i != (n - 1))
 		{
-			printf("%d", a[i]);
+			printf("%d, ", a[i]);
 			
 		}
 		else
@@ -21,7 +21,6 @@ void print_array(int *a, int n)
 			printf("%d", a[i]);
 		}
 
-		printf(", ");
 
 	}
 	printf("\n");

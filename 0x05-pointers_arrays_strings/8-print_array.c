12aadaa87e3def766c1a2ec2342c7ef37bcaff6b
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-20 14:26:15 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,13 +3,11 @@
  * print_array - entry point
  * @a: no.of array
  * @n: no. of elements
- * 
  * Return: void
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))

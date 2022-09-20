232faa8c9ee9d508e9c513112fceed81c6790dd7
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-20 13:51:19 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * print_array - entry point 
+ * print_array - entry point
  * @a: no.of array
  * @n: no. of elements
  * Return: void
@@ -11,14 +11,17 @@ void print_array(int *a, int n)
 	
 	for (i = 0; i < n; i++)
 	{
-		if (i != (n -1))
+		if (i != (n - 1))
+		{
+			printf("%d", a[i]);
+			
+		}
+		else
 		{
 			printf("%d", a[i]);
 		}
-		else 
-		{
-			printf("%d", a[i]);
-		}
+
+		printf(", ");
 
 	}
 	printf("\n");

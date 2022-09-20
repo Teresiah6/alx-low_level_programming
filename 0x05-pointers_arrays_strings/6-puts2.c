d5123a84f0536142f42edc6ba9c19755aac7c910
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-20 08:40:37 -0700

    chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: string whose chars to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+
+	_putchar('\n');
+}

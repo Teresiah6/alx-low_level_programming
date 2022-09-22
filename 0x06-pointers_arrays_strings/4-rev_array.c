5f0d4e513bc8193c2d6e5f965f51711335f81ddf
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-22 07:44:04 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,9 +12,8 @@ void reverse_array(int *a, int n)
 
 	for (half = n / 2; half > 0; half --, i++)
 	{
-
 		a[n - i -1] += a[i];
 		a[i] = a[n - i - 1] - a[i];
-		a[n -i - 1] = a[n - i - 1] - a[i];	
-	}	
+		a[n -i - 1] = a[n - i - 1] - a[i];
+	}
 }

4da3416cd4f9b5d92a8343aa9090881d9945ffea
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-22 08:03:21 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -strcmp - entry point
+ * _strcmp - entry point
  *  @s1: string 1 to compare
  *  @s2: string 2 to compare
  *  Return: an int
@@ -11,10 +11,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-	if (s1[i] != s2[i])
-	{
-		return(s1[i] - s2[i]);
-	}
+		if (s1[i] != s2[i])
+		{
+			return(s1[i] - s2[i]);
+		}
 	}
 
 	return(0);

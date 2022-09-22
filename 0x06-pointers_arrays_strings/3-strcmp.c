c331ff43d5b299fa0cda750c1142e3bef42cee6c
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-22 07:23:37 -0700

    strcomp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		return(0);
-	}
-	else if (s1 < s2)
+	if (s1[i] != s2[i])
 	{
-		return(-1);
+		return(s1[i] - s2[i]);
 	}
-	else if (s1 > s2)
-	{
-		return(1);
 	}
+
+	return(0);
+
 }

175f49c239bfea24106b377a37b743456139ecca
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-22 07:41:26 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - entry point
+ * @a: string to reverse
+ * @n: no. of elements of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int half;
+
+	for (half = n / 2; half > 0; half --, i++)
+	{
+
+		a[n - i -1] += a[i];
+		a[i] = a[n - i - 1] - a[i];
+		a[n -i - 1] = a[n - i - 1] - a[i];	
+	}	
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
-ifndef HEADER_FILE
+#ifndef HEADER_FILE
 #define HEADER_FILE
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif
+

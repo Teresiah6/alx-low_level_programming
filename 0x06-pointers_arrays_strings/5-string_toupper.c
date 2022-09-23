a6bf161a870800b2147744196dd3fabb0b34c088
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-23 05:35:24 -0700

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,8 +4,11 @@
  * @str: string to convert
  * Return: return char
  */
+#define MAX_SIZE 100
+
 char *string_toupper(char *str)
 {
+	char str[MAX_SIZE];
 	int i;
 	
 	for (i = 0; str[i] != '\0'; i++)

c8a9bdefba0a6f3c73a658663479d74db70135f4
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-26 11:48:43 -0700

    readme and memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - entry point
+ * @s: points to block of memory to fill
+ * @n: number pf bytes to set value
+ * @b: value to set
+ * Return: char to be returned
+ */
+char *_memset(char *s, char b,unsigned int n)
+{
+	unsigned int i;
+	
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+
+
+	return(s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif

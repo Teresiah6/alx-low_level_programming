c6018311da67872487b7ac1dee96a55535672819
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 12:25:27 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] < '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 
-		else if ((s[i] != c) && (s[i] == '\0'))
-			return (NULL);
+/*		else if ((s[i] != c) && (s[i] == '\0'))
+			return (NULL); */
 
 	}
 

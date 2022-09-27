710b97bc3396059f02005869cbf23b0ae016f264
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 08:42:24 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,10 @@
 #define NULL 0
 /**
  * *_memcpy - entry point
- * @dest: to be copied to 
+ * @dest: to be copied to
  * @src: to be copied from
  * @n:bytes of memory to be copied
- * Return: pointer to dest
+ * Return: pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {

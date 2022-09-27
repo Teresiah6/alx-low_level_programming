04686f387970baadb4daa654bde73581bcbe0e4a
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 08:02:51 -0700

    pront diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,27 +1,24 @@
 #include "main.h"
 /**
  * print_diagsums - entry point
- * @size:
- * @a:
+ * @size: 2d array to find sum for
+ * @a: size of an array
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum;
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
 
 
 	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
-		{
-			if(a[i] == a[j])
-			{
-				sum = [i + j];
-			}
-		}
-
-		sum++;
+		sum1 += a[i];
+		sum2 += a[size - i -1];
+		a +=size;
 	}
 
-	_putchar(sum);
+	_putchar(sum1);
+	_putchar(sum2);
 }

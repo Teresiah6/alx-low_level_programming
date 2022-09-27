c8c8aefbe403d4226970d70cd0600eab199fc1a1
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 04:35:20 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,23 +7,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-	unsigned int sim = 0;
+	int i, j, sim = 0;
 
-	for (i = 0, s[i] <= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		for (j = 0; accept[j] < '\0'; j++)
+		for (j = 0; accept[j] > '\0'; j++)
 		{
-			if(s[i] == accept[j])
+			if (s[i] == accept[j])
 			{
 				sim++;
 				break;
 			}
 		}
-		
 		if (accept[j] == '\0')
+		{
 			break;
+		}
+		
 	}
 
-	return(sim);
+	return (sim);
 }

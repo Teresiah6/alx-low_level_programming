2e243c9974988832ca3e7575889ec74de9cc95ab
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 08:20:19 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string: entry point
+ * @s: char double pointer
+ * @to: pointer
+ * Return: void nothing to return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

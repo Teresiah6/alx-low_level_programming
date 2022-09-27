0d2fdbb04afab166ba02aaca88555d06932027be
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-27 07:51:57 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 /**
  * print_chessboard - entry point
  * @a: pointer to array column
- * Return: void
+ * Return: void nothing to return
  */
 void print_chessboard(char (*a)[8])
 {
@@ -15,9 +15,9 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j ++)
 		{
-			_putchar (a[i][j]);
+			_putchar(a[i][j]);
 		}
-		_putchar ('\n');
+		_putchar('\n');
 
 	}
 }

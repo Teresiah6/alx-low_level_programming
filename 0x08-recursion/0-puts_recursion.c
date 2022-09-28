8b88bb829f33abee8bde575fcb4589ef990fccd9
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 11:12:53 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar(s);
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 
 #endif

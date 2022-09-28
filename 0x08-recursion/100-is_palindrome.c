14a22b217f4e944fc38ee30b18edbd2bb8c9d652
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 13:16:16 -0700

    optional

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * is_palindrome - entry point
+ * @s: string
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * wildcmp - entry point:
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if identical else 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+}

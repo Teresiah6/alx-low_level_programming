246924c5befea3e2533c141dec8b4e67b308b363
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 12:37:02 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - entry point 
+ * @n:number to get sqrt of 
+ * Return: int sqrt of number
+ */
+int _sqrt_recursion(int n)i
+{
+	if (n < 0)
+		return (-1);
+	if( n == 1)
+		return (1);
+
+	return (n * _sqrt_recursion(n - 1));
+}

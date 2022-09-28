75686b14bd3c8af3c1ffdefea0bc9e637f54140a
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 12:16:34 -0700

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - entry point
+ * @x: first int
+ * @y: second int
+ * Return: return @x^@y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if(y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1);
+}

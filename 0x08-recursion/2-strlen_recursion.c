d47c1568c17c5e705828246d84996bda74281934
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 11:34:44 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,14 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if(*s)
+	if (*s)
 	{
 		len++;
-		len += strlen_recursion(s + 1); 
+		len += _strlen_recursion(s + 1); 
 	}
 
 
 
 
-	return(len);
+	return (len);
 }

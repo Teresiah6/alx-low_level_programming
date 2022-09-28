8e6c4ab3200c3f31e7b12e2ef6c1c62e07db4252
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-28 10:56:24 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string and new line
+ * @s: pointer to the string
+ * return: void 
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+void _puts_recursion(char *s);
+
+#endif

ac0617ff198b1dd63697a8cd9ef812753bdb9126
Teresiah6 <wangariteresiah6@gmail.com>
2022-09-30 12:46:23 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: 
+ * @argv:
+ * return: void
+ */
+int main (int argc, char* argv[])
+{
+	_putchar (argv[0]);
+
+}

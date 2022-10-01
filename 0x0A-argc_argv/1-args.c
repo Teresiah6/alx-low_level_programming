0463ecadfaf62fd382598462d3655efbdb45ede7
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-01 00:35:20 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero
+ */
+int main(int argc, char* argv[])
+{
+	printf("argc = %d\n", argc);
+	
+	return (0);
+}

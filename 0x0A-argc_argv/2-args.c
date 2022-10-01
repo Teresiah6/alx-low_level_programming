b8f8ba063969c024d1392102fa883bd3d8ea030b
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-01 00:49:15 -0700

    list of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: zero success
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i];
+	}
+
+	return (0); 
+}

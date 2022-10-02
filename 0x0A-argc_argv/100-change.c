c60f115a8cab58504ec8b41ccf03e74d5f40100d
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-02 01:34:36 -0700

    add

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,52 +4,47 @@
 /**
  * isdigit - check if an integer
  * @d: string to check
- * Return: 1 or 2
+ * Return: 1 or 0
  */
-int isdigit(char *s)
+int isInteger(char *s)
 {
 	int i = 0;
-
-	while (d[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if (d[i] < '0' || d[i] > '9')
-		return (0);
-	i++;
+		 if (s[i] < '0' || s[i] > '9')
+			 return (0);
+		         i++;
 	}
 
 	return (1);
 }
+
 /**
- * main - entry point
- * @arg: argument count
- * @argc: argument vector
- * Return: zero
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	int i = 0;
-	int coinUsed = 0;
-	int coin = 0;
+	int i = 0, coinUsed = 0, coin = 0;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if(argc != 2)
+	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+		        printf("Error\n");
+			        return (1);
 	}
-
-	if (isdigit(argv[1]))
+	if (isInteger(argv[1]))
 	{
-		i = atoi (argv[1]);
-
+		i = atoi(argv[1]);
 		while (i > 0 && coin <= 4)
 		{
 			if (i >= coins[coin])
-			{
-				i -= coins[coin];
-				coinUsed++;
+			{													                        i -= coins[coin];
+																                        coinUsed++;
 			}
-			else 
+	                else
 			{
 				coin++;
 			}

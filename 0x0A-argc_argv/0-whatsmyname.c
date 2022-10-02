54755b8cbab90c3211eb7090cc4e52720b24757b
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-02 02:32:13 -0700

    what's my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,11 +5,12 @@
  * main - entry point
  * @argc: count of argv
  * @argv: pointer of array
- * return: void
+ * Return: always 0
  */
 int main(int argc, char* argv[])
 {
 	(void) argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);

3cbeb136669e3b8d0100d10fa4da0438c211bdd5
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-02 01:59:07 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,11 +9,12 @@
 int isInteger(const char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
-		        if (s[i] < '0' || s[i] > '9')
-				                return (0);
-			        i++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+			 i++;
 	}
 	return (1);
 }
@@ -31,24 +32,25 @@ int main(int argc, char const *argv[])
 
 	if (argc != 2)
 	{
-		        printf("Error\n");
-			        return (1);
+		printf("Error\n");
+		return (1);
 	}
 	if (isInteger(argv[1]))
 	{
-		        i = atoi(argv[1]);
-			        while (i > 0 && coin <= 4)
-					        {
-							                if (i >= coins[coin])
-										                {
-													                        i -= coins[coin];
-																                        coinUsed++;
-																			                }
-									                else
-												                {
-															                        coin++;
-																		                }
-											        }
+		i = atoi(argv[1]);
+
+		while (i > 0 && coin <= 4)
+		{
+			if (i >= coins[coin])
+			{ 
+				i-= coins[coin];
+				coinUsed++;
+			}
+			else
+			{
+				coin++;
+			}
+		}
 	}
 
 	printf("%i\n", coinUsed);

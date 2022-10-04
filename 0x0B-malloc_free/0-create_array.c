ac84f74ec3e0c4317ccdfde3d9630bde7b4cc9fb
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-04 02:09:15 -0700

    init, 0, 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *create_array - create [] of chars
+ * @size: size of array
+ * @c: character being initialized
+ * Return:char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *m = malloc(size);
+
+	if (size = 0 || m ==0)
+	{
+		return (0);
+	}
+
+	while (size--)
+		m[size] = c;
+
+	return (m);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * *_strdup -pointer to new string
+ * @str -pointer to string
+ * Return: characters
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	size =0;
+	char *m;
+
+
+/*does string have anything*/
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+/*increase size of string if end is not reached*/
+	for (; str[size] != '\0'; size++);
+	return (str);
+
+	/*string allocation*/
+	m = malloc(size * sizeof(*str) + 1);
+
+	if(m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+
+	return (m);
+}

b09722e8cfd6301c6a7bb0a7f224f6a2230af2b0
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-04 03:31:29 -0700

    concantenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,8 +8,9 @@
  */
 int _strlen(char *s)
 {
-	int size = 0;
-	for(; s[size] = '\0'; size++);
+	int size;
+
+	for(size = 0; s[size] = '\0'; size++);
 
 	return (size);
 
@@ -35,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 
-	m = malloc((size1 + size 2) * sizeof(char) + 1);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
 		return (0);
 

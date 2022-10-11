6bc503cf6c91cd0ea29fa64db7818026821e2b46
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-11 09:41:38 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_dog - entry point dog data 
+ * print_dog - entry point dog data
  * @d: struct dog
  */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n",(*d).name != NULL ? d->name : "(nil)",(*d).age,(*d).owner != NULL ? d->owner : "(nil)");
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				(*d).name != NULL ? d->name : "(nil)",
+				(*d).age,
+				(*d).owner != NULL ? d->owner : "(nil)");
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,8 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+}
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

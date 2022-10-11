cd29af390be0d76ef702baf8ad00837e851464a0
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-11 08:03:24 -0700

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - definition of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t

cca1cee0cbec03eabb395d9b0676c0cdd5264208
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-11 08:33:29 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - entry point dog data 
+ * @d: struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",(*d).name != NULL ? d->name : "(nil)",(*d).age,(*d).owner != NULL ? d->owner : "(nil)");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,5 @@ typedef struct dog
 	char *owner;
 }dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif

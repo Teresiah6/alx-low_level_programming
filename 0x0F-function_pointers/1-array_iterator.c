aec54e3ca798653156e659ec36d00c25166af8c8
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-13 13:06:07 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include <stddef.h> 
+#include <stddef.h>
 #include <stdlib.h>
 /**
  * array_iterator - entry array
+ * @array: array of numbers
  * @size: size of the array
  * @action: pointer to function needed
  * return: void

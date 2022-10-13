6aeddb8e781b14a7f7e61da9f468e8d8c1002680
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-13 12:51:42 -0700

    print my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));

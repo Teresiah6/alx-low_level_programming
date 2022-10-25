0f20d5ebb5b5b8a023c14c6c722ab39c209f6c65
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-25 12:59:10 -0700

    printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-
+/**
+ *
+ */
+(void) __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}

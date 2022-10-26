42dcc69374cf7f3b6b2f6b9b9dac30723e2591f7
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-26 11:00:51 -0700

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * bmain - function to print before main
  *
  */
-void bmain (void)__attribute__((constructor));
+void bmain(void)__attribute__((constructor));
 void bmain(void)
 {
 	printf("You're beat! and yet, you must allow,");

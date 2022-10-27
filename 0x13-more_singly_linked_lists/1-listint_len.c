f967d65b52819a39c953a4fb6368f7bbccb19b42
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-27 09:00:15 -0700

    number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * listint_len - number of elements
+ * @h: linked list
+ * Return: number of elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t ll;
+
+	ll = 0;
+
+	if (h != NUll)
+	{
+		h = h->next;
+		ll++;
+	}
+
+	return (ll);
+}

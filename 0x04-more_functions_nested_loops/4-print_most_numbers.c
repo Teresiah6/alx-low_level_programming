b5eea3ce37cc8445b60c2475c24afb1c433210f4
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-29 04:06:43 -0700

    print most no.s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,27 +2,16 @@
 /**
  * print_most_numbers - entry point
  *
- * @i: prints no.s 0 to 9 less 2 & 4
- *
  * Return: void
  */
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if ((i != 2) && (i != 4))
-		{
-			continue;
-		}
-
-		else
-			break;
-
-		_putchar (i);
-
+		if ((i != 50) && (i != 52))
+			_putchar (i);
 	}
-
 	_putchar ('\n');
 }

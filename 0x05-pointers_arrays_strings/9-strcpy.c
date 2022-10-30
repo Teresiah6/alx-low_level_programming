baa1a74ae653820190e6d080c05e898ee38f4fd5
Teresiah6 <wangariteresiah6@gmail.com>
2022-10-30 02:06:15 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * *_strcpy - entry point
+ * @dest: destinattion string
+ * @src: source string
  * Return: return a char
  */
-char *strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	return(dest);
+	return (dest);
 }

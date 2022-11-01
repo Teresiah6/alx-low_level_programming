606cacd793a2f8b6e28b8f057f869922cf3ad9c0
Teresiah6 <wangariteresiah6@gmail.com>
2022-11-01 12:22:41 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,4 +7,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	int count, i = 0;
+	unsigned long int current, excl = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = excl >> 1;
+		if (current & 1)
+			count++;
+	}
+	return (count);
 }

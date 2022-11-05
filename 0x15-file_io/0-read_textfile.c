ec500854ba00d9c0acd3ea89b7084d9de4f3f711
Teresiah6 <wangariteresiah6@gmail.com>
2022-11-05 13:37:07 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - read text and print it to POSIX
+ * @filename: file 
+ * @letters: number of letters to print
+ * Return: actual numberof letters to read & print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fl, fread,fwrite;
+	char *ttlsize;
+
+	ttlsize = malloc(sizeof(char) *letters);
+
+	if (ttlsize == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+
+	fl = open (filename, O_RDONLY);
+	if (fl == -1)
+		return (0);
+	fread = read(fl, ttlsize, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, ttlsize, fread);
+
+	if (fwrite == -1)
+		return (0);
+
+	close(file);
+	free(ttlsize);
+	return (fwrite);
+}

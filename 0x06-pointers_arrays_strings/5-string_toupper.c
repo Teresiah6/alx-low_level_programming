271055c5d83956edee504cf4c415b7aba6775616
Teresiah6 <wangariteresiah6@gmail.com>
2022-11-07 04:47:04 -0800

    string upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,13 +11,11 @@ char *string_toupper(char *s)
 	while (*(s + i) != '\0')
 	{
 		if ((*(s + i) >= 97) && (*(s + i) <= 122))
-			{
-				*(s + i) = *(s + i) - 32;
-			}
-
-		i++
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+		i++;
 	}
 
-	
 	return (s);
 }

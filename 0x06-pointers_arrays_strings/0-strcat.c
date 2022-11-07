382ace6bc4f42d5d1f0c54016d06e530d4cf2655
Teresiah6 <wangariteresiah6@gmail.com>
2022-11-07 03:51:06 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,13 @@ char *_strcat(char *dest, char *src)
 	while (dest[i] != '\0')
 	{
 		i++;
-		while (src[j] != '\0')
-		{
+	}
+	while (src[j] != '\0')
+	{
 			dest[i] = src[j];
 			j++;
 			i++;
-		}
-		dest[i] = '\0';
 	}
+	dest[i] = '\0';
 	return (dest);
 }

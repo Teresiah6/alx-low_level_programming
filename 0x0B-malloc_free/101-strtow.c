b1129d3307a4cf55f7536306d83d9e409e61e2f5
Teresiah6 <wangariteresiah6@gmail.com>
2022-11-22 14:07:40 -0800

    strtow

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,108 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * isSpace - if char is space or not
+ * @c: input char
+ * Return: 1 true or0 not
+ */
+int isSpace(char c)
+{
+	return (c == ' ');
+}
+/**
+ * startIndex - start index of non-space char
+ * @s: input str                                              * @index: starting index
+ * Return: index of first non-space char
+ */
+
+int startIndex(char *s, int index)
+{
+	while (isSpace(*(s + index)))
+		index++;
+	return (index);
+}
+
+/**
+ * endIndex - returns last index of non-space char
+ * @s: input string
+ * @index: starting index
+ * Return: index of last index of non-space char
+ */
+
+int endIndex(char *s, int index)
+{
+	while (!isSpace(*(s + index)))
+		index++;
+	return (index);
+}
+
+/**
+ * countWords - counts numbers of words in string
+ * @s: input string
+ * Return: number of words
+ */
+int countWords(char *s)
+{
+	int wordOn = 0;
+	int words = 0;
+
+	while (*s)
+	{
+		if (isSpace(*s) && wordOn)
+			wordOn = 0;
+		else if (!isSpace(*s) && !wordOn)
+		{
+			wordOn = 1;
+			words++;
+		}
+		s++;
+	}
+	return (words);
+}
+
+/**
+ * strtow - splits str into words
+ * @str: str of words to be split
+ * Return: double pointer to str
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words =  countWords(str);
+
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+
+	}
+	ptr[i] = NULL;
+	return (ptr);
+}
